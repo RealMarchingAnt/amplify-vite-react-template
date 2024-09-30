f1911d182175826f6276b34c51be4058a8dd5e07
RealMarchingAnt <patrik.marchingant.johansson@gmail.com>
2024-09-30 23:25:49 +0200

    added bootstrap styling etc

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import { Authenticator } from "@aws-amplify/ui-react";
 import '@aws-amplify/ui-react/styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 const client = generateClient<Schema>();
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [newTodo, setNewTodo] = useState<string>("");
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -17,7 +25,20 @@ function App() {
   }, []);
 
   function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
+    if (!newTodo) return;
+    client.models.Todo.create({ content: newTodo });
+    setNewTodo("");
+  }
+
+  function validateTodo(e: React.ChangeEvent<HTMLInputElement>) {
+    // TODO do some basic validation
+    setNewTodo(e.target.value);
+  }
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {''
+    // Prevent the browser from reloading the page
+    e.preventDefault();
+    createTodo();
   }
 
   function deleteTodo(id: string) {
@@ -28,22 +49,43 @@ function App() {
     <Authenticator>
       {({ signOut, user }) => (
           <main>
-            <h1>{user?.signInDetails?.loginId}'s todos</h1>
+            <h1>{user?.signInDetails?.loginId}'s todo</h1>
             <h1>My todos</h1>
-            <button onClick={createTodo}>+ new</button>
-            <ul>
+            {/* <ul>
               {todos.map((todo) => (
                 <li key={todo.id}>{todo.content} <button onClick={() => deleteTodo(todo.id)}>- Delete</button></li>
               ))}
-            </ul>
-            <div>
-              🥳 App successfully hosted. Try creating a new todo.
+            </ul> */}
+            <Container>
+              <form onSubmit={handleSubmit}>
+                <Row>
+                  <Col md={6}>
+                    <Form.Control type="text" value={newTodo} onChange={validateTodo} placeholder="Enter todo" />
+                    {/* <input type="text" value={newTodo} onChange={validateTodo} placeholder="Enter todo" /> */}
+                  </Col>
+                  <Col md={{ span: 2, offset: 4 }}>
+                    <Button variant="primary" onClick={createTodo}>Create</Button>{' '}
+                  </Col>
+                </Row>
+              </form>
               <br />
-              <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-                Review next step of this tutorial.
-              </a>
+              {todos.map((todo) => (
+                <Row key={todo.id} >
+                  <Col md={8}>{todo.content}</Col>
+                  <Col md={{ span: 2, offset: 2 }}>
+                    <Button variant="danger" onClick={() => deleteTodo(todo.id)}>Delete</Button>{' '}
+                  </Col>
+                </Row>
+              ))}
+            </Container>
+
+
+            <div>
+              🥳 Welcome! Try creating a new todo.
+              <br />
+              <br />
             </div>
-            <button onClick={signOut}>Sign out</button>
+            <Button onClick={signOut}>Sign out</Button>
           </main>
 
       )}
