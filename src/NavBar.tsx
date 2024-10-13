6945878666628fefb01dcdaeb7c4b12dcefddf37
RealMarchingAnt <patrik.marchingant.johansson@gmail.com>
2024-10-13 22:48:20 +0200

    add collapse to navbar and change it to expand from lr to sm

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,5 +1,4 @@
-import Nav from "react-bootstrap/Nav";
-import { Navbar } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 
 interface NavBarProps {
   username: string;
@@ -7,24 +6,25 @@ interface NavBarProps {
 }
 
 const NavBar: React.FC<NavBarProps> = ({ username, signOut }) => (
-  <Navbar bg="white" variant="light" fixed="top" expand="lg" className="w-100">
+  <Navbar bg="white" variant="light" fixed="top" expand="sm" className="w-100">
     <Navbar.Brand href="#home">
-    <img
+      <img
         src="/logo.svg"
         width="30"
         height="30"
         className="d-inline-block align-top"
         alt="Todos"
-    />
-    <span className="ml-2">Todos</span>
+      />
+      <span className="ml-2">Todos</span>
     </Navbar.Brand>
-    <Navbar.Collapse className="justify-content-end">
-        <Nav className="mr-auto">
-          <Navbar.Text>
-            Logged in as {username}
-          </Navbar.Text>
-          <Nav.Link role="underline" onClick={signOut}>Sign out</Nav.Link>
-        </Nav>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+      <Nav className="mr-auto">
+        <Navbar.Text>
+          Logged in as {username}
+        </Navbar.Text>
+        <Nav.Link role="button" onClick={signOut}>Sign out</Nav.Link>
+      </Nav>
     </Navbar.Collapse>
   </Navbar>
 );
